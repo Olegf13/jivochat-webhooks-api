e7165161282bfac1924e587338182078b0073767
Oleg Fedorov <olegf39@gmail.com>
2017-04-21 21:49:21 +0300

    Add `Page` class.
    Holds information about a web page on which the visitor (URL and page title).

---
+++ src/Request/Page.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jivochat\Webhooks\Request;
+
+use Jivochat\Webhooks\PopulateObjectViaArray;
+
+/**
+ * Information about a page on which the visitor.
+ *
+ * @package Jivochat\Webhooks\Request
+ */
+class Page
+{
+    use PopulateObjectViaArray;
+
+    /** @var string URL of the page where the user is located (e.g. "http://example.com/"). */
+    public $url;
+    /** @var string Page Title (e.g. "Page title"). */
+    public $title;
+}
