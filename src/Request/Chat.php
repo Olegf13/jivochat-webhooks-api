94219b364e3313c42c482b629b59dce55d94ad78
Oleg Fedorov <olegf39@gmail.com>
2017-04-21 21:39:31 +0300

    Add `Chat` class.
    `Chat` object holds data (chat rate and messages list) on completed chatting.

---
+++ src/Request/Chat.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jivochat\Webhooks\Request;
+
+use Jivochat\Webhooks\PopulateObjectViaArray;
+
+/**
+ * Holds data on completed chatting.
+ *
+ * @package Jivochat\Webhooks\Request
+ */
+class Chat
+{
+    use PopulateObjectViaArray;
+
+    /** @var Message[] Messages list. See {@link Message} for details. */
+    public $messages;
+    /** @var string|null User chat rank ("positive"|"negative"|null). */
+    public $rate;
+}
