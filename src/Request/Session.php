75e5f5ae76cee139e040476d29a977af72075d64
Oleg Fedorov <olegf39@gmail.com>
2017-04-21 21:56:46 +0300

    Add `Session` class.
    Holds user session information (IP, "user agent" etc).

---
+++ src/Request/Session.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Jivochat\Webhooks\Request;
+
+use Jivochat\Webhooks\PopulateObjectViaArray;
+
+/**
+ * User session information (IP, "user agent" etc).
+ *
+ * @package Jivochat\Webhooks\Request
+ */
+class Session
+{
+    use PopulateObjectViaArray;
+
+    /** @var GeoIP Data from geoip. See {@link GeoIP} for details. */
+    public $geoip;
+    /** @var string|null utm (e.g. null). */
+    public $utm;
+    /** @var string IP address (e.g. "208.80.152.201"). */
+    public $ip_addr;
+    /** @var string User agent info. (e.g. "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"). */
+    public $user_agent;
+}
