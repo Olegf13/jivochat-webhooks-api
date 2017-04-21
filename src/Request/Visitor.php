e3804c1a84509fdb1315fa363de447f4d9bfcc07
Oleg Fedorov <olegf39@gmail.com>
2017-04-21 21:46:33 +0300

    Add `Visitor` class.
    Holds information about the visitor (name, email, phone etc).

---
+++ src/Request/Visitor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Jivochat\Webhooks\Request;
+
+use Jivochat\Webhooks\PopulateObjectViaArray;
+
+/**
+ * Information about the visitor.
+ *
+ * @package Jivochat\Webhooks\Request
+ */
+class Visitor
+{
+    use PopulateObjectViaArray;
+
+    /** @var string|null Visitor name (e.g. "John Smith"). */
+    public $name;
+    /** @var string|null Visitor email (e.g. "email@example.com"). */
+    public $email;
+    /** @var string|null Visitor phone (e.g. "+14084987855"). */
+    public $phone;
+    /** @var int Visitor number (e.g. 2067). */
+    public $number;
+    /** @var string|null Additional information about the client (e.g. "Description text"). */
+    public $description;
+    /** @var array|null Information about visitor's social accounts (e.g. null). */
+    public $social;
+    /** @var int Number of visitor's chats (e.g. 5). */
+    public $chats_count;
+}
