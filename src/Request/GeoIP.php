0a9d26cdba313cf1f0867a6e25c4cc8adf41df36
Oleg Fedorov <olegf39@gmail.com>
2017-04-21 21:44:03 +0300

    Add `GeoIP` class (holds visitor's geo data).

---
+++ src/Request/GeoIP.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Jivochat\Webhooks\Request;
+
+use Jivochat\Webhooks\PopulateObjectViaArray;
+
+/**
+ * Data from geoip.
+ *
+ * @package Jivochat\Webhooks\Request
+ */
+class GeoIP
+{
+    use PopulateObjectViaArray;
+
+    /** @var string Area code (e.g. "CA"). */
+    public $region_code;
+    /** @var string ISO country code (e.g. "US"). */
+    public $country_code;
+    /** @var string Country name (e.g. "United States"). */
+    public $country;
+    /** @var string Region (e.g. "California"). */
+    public $region;
+    /** @var string City (e.g. "San Francisco"). */
+    public $city;
+    /** @var string Unknown, empty string (e.g. ""). */
+    public $isp;
+    /** @var string Latitude (e.g. "37.7898"). */
+    public $latitude;
+    /** @var string Longitude (e.g. "-122.3942"). */
+    public $longitude;
+    /** @var string Company name (e.g. "Wikimedia Foundation"). */
+    public $organization;
+}
