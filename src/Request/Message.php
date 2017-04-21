05afa5e130adcb8ebcab87841037973a1f19fe65
Oleg Fedorov <olegf39@gmail.com>
2017-04-21 22:06:30 +0300

    Add `Message` class.
    Holds chat message information (timestamp, message etc).

---
+++ src/Request/Message.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Jivochat\Webhooks\Request;
+
+use Jivochat\Webhooks\PopulateObjectViaArray;
+
+/**
+ * Chat message.
+ *
+ * @package Jivochat\Webhooks\Request
+ */
+class Message
+{
+    use PopulateObjectViaArray;
+
+    /** @var int Timestamp of the message receipt (e.g. null). */
+    public $timestamp;
+    /** @var string Message Type ("visitor" - a message from a client, "agent" - a message from an agent). */
+    public $type;
+    /** @var int|null Agent ID, which responded to the message (exists only if {@link type} = "agent"). */
+    public $agent_id;
+    /** @var string Message body (e.g. "Hi, can I ..."). */
+    public $message;
+    /** @var bool|null A sign that the user was added to the black list (e.g. null). */
+    public $blacklisted;
+}
