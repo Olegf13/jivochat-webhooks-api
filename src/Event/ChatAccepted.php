8c4f91ab724c4fc7f75d33c42914315476c52a72
Oleg Fedorov <olegf39@gmail.com>
2017-04-24 14:48:23 +0300

    Implement `ChatAccepted` event class.

---
+++ src/Event/ChatAccepted.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Jivochat\Webhooks\Event;
+
+use Jivochat\Webhooks\Request\Agent;
+
+/**
+ * Class ChatAccepted
+ *
+ * Event will be sent when agent clicks 'Reply'.
+ *
+ * All known data about visitor and some agent's info will be sent in the request parameters.
+ * Also parameters including visitor's id if it was sent to the widget using `jivo_api.setUserToken`.
+ *
+ * If response to `chat_accepted` contains contact_info,
+ * this data will be displayed to the agent as if a visitor introduced in the chat window.
+ * It's also will be saved in the archive and email with the chat log.
+ *
+ * @package Jivochat\Webhooks\Event
+ */
+class ChatAccepted extends Event
+{
+    /** @var int Chat id (e.g. 7180). */
+    protected $chat_id;
+    /** @var Agent Object with information about the operator. See {@link Agent} for details. */
+    protected $agent;
+
+    /**
+     * Setter for {@link agent} property.
+     *
+     * @param Agent|array $data
+     * @throws \InvalidArgumentException
+     */
+    public function setAgent($data)
+    {
+        if (is_array($data)) {
+            $agent = new Agent();
+            $agent->populate($data);
+            $this->agent = $agent;
+            return;
+        }
+
+        if ($data instanceof Agent) {
+            $this->agent = $data;
+            return;
+        }
+
+        throw new \InvalidArgumentException('Invalid data given.');
+    }
+}
