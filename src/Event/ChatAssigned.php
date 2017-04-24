90db1b3f63b856d9899bf256bd64c9d08b2ec3b9
Oleg Fedorov <olegf39@gmail.com>
2017-04-24 14:48:38 +0300

    Implement `ChatAssigned` event class.

---
+++ src/Event/ChatAssigned.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Jivochat\Webhooks\Event;
+
+use Jivochat\Webhooks\Request\Agent;
+
+/**
+ * Class ChatAssigned
+ *
+ * Event will be sent when a chat connects to CRM using the parameter "crm_link" from reply on Chat Accepted.
+ *
+ * All known data about visitor and some agent's info will be sent in the request parameters.
+ * Also parameters including visitor's id if it was sent to the widget using `jivo_api.setUserToken`.
+ *
+ * In response we expect only `{"result": "ok or an error message"}`.
+ *
+ * @package Jivochat\Webhooks\Event
+ */
+class ChatAssigned extends Event
+{
+    /** @var int Chat id (e.g. 7180). */
+    protected $chat_id;
+    /** @var Agent Object with information about the operator. See {@link Agent} for details. */
+    protected $agent;
+    /** @var string CRM link from the event Chat_accepted (e.g. "..."). */
+    protected $assign_to;
+
+    /**
+     * Setter for {@link agent} property.
+     *
+     * @param Agent|array $data
+     * @throws \InvalidArgumentException
+     */
+    public function setAgent($data)
+    {
+        if (is_array($data)) {
+            $agent = new Agent();
+            $agent->populate($data);
+            $this->agent = $agent;
+            return;
+        }
+
+        if ($data instanceof Agent) {
+            $this->agent = $data;
+            return;
+        }
+
+        throw new \InvalidArgumentException('Invalid data given.');
+    }
+}
