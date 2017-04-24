f6332f1766da03aa88addde4c4d1aa59436f8cb8
Oleg Fedorov <olegf39@gmail.com>
2017-04-24 14:54:45 +0300

    Update `EventListener` - remove `EVENT_*` constants (moved to abstract `Event` class), update imports (due to namespace things).

--- src/EventListener.php
+++ src/EventListener.php
@@ -2,75 +2,17 @@
 
 namespace Jivochat\Webhooks;
 
+use Jivochat\Webhooks\Event\Event;
+use Jivochat\Webhooks\Log\LogInterface;
+
 /**
  * Class EventListener
+ * todo: describe me
  *
  * @package Jivochat\Webhook
  */
 class EventListener
 {
-    /**
-     * Event will be sent when agent clicks 'Reply'.
-     *
-     * All known data about visitor and some agent's info will be sent in the request parameters.
-     * Also parameters including visitor's id if it was sent to the widget using `jivo_api.setUserToken`.
-     *
-     * If response to `chat_accepted` contains contact_info,
-     * this data will be displayed to the agent as if a visitor introduced in the chat window.
-     * It's also will be saved in the archive and email with the chat log.
-     */
-    const EVENT_CHAT_ACCEPTED = 'chat_accepted';
-
-    /**
-     * Event will be sent when a chat connects to CRM using the parameter "crm_link" from reply on Chat Accepted.
-     *
-     * All known data about visitor and some agent's info will be sent in the request parameters.
-     * Also parameters including visitor's id if it was sent to the widget using `jivo_api.setUserToken`.
-     *
-     * In response we expect only `{"result": "ok or an error message"}`.
-     */
-    const EVENT_CHAT_ASSIGNED = 'chat_assigned';
-
-    /**
-     * Event will be sent when a chat is closed in the agent application.
-     *
-     * All known data about visitor, agent's info and the chat log will be sent in the request parameters.
-     * Also parameters including visitor's id if it was sent to the widget using `jivo_api.setUserToken`.
-     *
-     * In response we expect only {"result": "ok or an error message"}
-     */
-    const EVENT_CHAT_FINISHED = 'chat_finished';
-
-    /**
-     * Event will be sent when a visitor's information has been updated
-     * (for example a visitor filled the contacts form in the chat).
-     *
-     * All known data about visitor and agent's info will be sent in the request parameters.
-     * Also parameters including visitor's id if it was sent to the widget using `jivo_api.setUserToken`.
-     *
-     * In response we expect only {"result": "ok or an error message"}
-     */
-    const EVENT_CHAT_UPDATED = 'chat_updated';
-
-    /**
-     * Event will be sent when a visitor sends an offline message through the chat offline form.
-     *
-     * All known data about visitor and offline message will be sent in the request parameters.
-     * Also parameters including visitor's id if it was sent to the widget using `jivo_api.setUserToken`.
-     *
-     * In response we expect only {"result": "ok or an error message"}
-     */
-    const EVENT_OFFLINE_MESSAGE = 'offline_message';
-
-    /** Available events list. */
-    const EVENTS = [
-        self::EVENT_CHAT_ACCEPTED,
-        self::EVENT_CHAT_ASSIGNED,
-        self::EVENT_CHAT_FINISHED,
-        self::EVENT_CHAT_UPDATED,
-        self::EVENT_OFFLINE_MESSAGE,
-    ];
-
     /** @var array Event listeners. */
     protected $listeners = [];
 
@@ -80,19 +22,19 @@ class EventListener
     /**
      * EventListener constructor.
      *
-     * @param ILogger[] $loggers Loggers to be used. Optional. Requires loggers to be {@link ILogger} descendants.
-     * @throws \InvalidArgumentException in case when invalid logger given (not a ILogger descendant).
+     * @param LogInterface[] $loggers Loggers to be used. Optional. Requires loggers to be {@link LogInterface} descendants.
+     * @throws \InvalidArgumentException in case when invalid logger given (not a LogInterface descendant).
      */
     public function __construct(array $loggers = [])
     {
         if (!empty($loggers)) {
             foreach ($loggers as $logger) {
-                if ($logger instanceof ILogger) {
+                if ($logger instanceof LogInterface) {
                     continue;
                 }
 
                 $class = get_class($logger);
-                throw new \InvalidArgumentException("Invalid Logger object given, ILogger descendant required, `{$class}` given.");
+                throw new \InvalidArgumentException("Invalid Logger object given, LogInterface descendant required, `{$class}` given.");
             }
         }
 
@@ -113,7 +55,7 @@ class EventListener
      */
     public function on(string $event, callable $callback): void
     {
-        if (!in_array($event, static::EVENTS, true)) {
+        if (!in_array($event, Event::EVENTS, true)) {
             throw new \InvalidArgumentException("Invalid `event` name given (`{$event}`).");
         }
 
@@ -166,28 +108,44 @@ class EventListener
             throw new \LogicException("Request doesn't contain `event` field (not a Jivochat Webhook?).");
         }
 
-        if (!in_array($event, static::EVENTS, true)) {
+        if (!in_array($event, Event::EVENTS, true)) {
             throw new \LogicException("Got unknown event name from Webhook request (`{$event}`).");
         }
 
+        // if no handler is registered on current event, respond with a default response
         if (!array_key_exists($event, $this->listeners)) {
-            $responseData = (new Response())->getResponse();
-        } else {
-            /** @var string Response JSON string. */
-            $responseData = call_user_func($this->listeners[$event], $decodedRequest);
-            if (empty($responseData)) {
-                throw new \LogicException("Event handler for `{$event}` event returned an empty response.");
-            }
+            return $this->respond(new Response());
         }
+
+        /** @var Response $response */
+        $response = call_user_func($this->listeners[$event], $decodedRequest);
+        if (!($response instanceof Response)) {
+            throw new \LogicException("Registered handler for `{$event}` event invalid response.");
+        }
+
+        return $this->respond(call_user_func($this->listeners[$event], $decodedRequest));
+    }
+
+    /**
+     * Responds on Webhook after event handler is executed.
+     *
+     * @param Response $response Event handler response.
+     * @return bool Returns `true` on success response.
+     * @throws \RuntimeException
+     */
+    protected function respond(Response $response): bool
+    {
+        /** @var string Webhook response JSON string. */
+        $responseJSON = $response->getResponse();
 
         // log response string via registered loggers
         foreach ($this->loggers as $logger) {
-            $logger->logResponse($responseData);
+            $logger->logResponse($responseJSON);
         }
 
         \HttpResponse::status(200);
         \HttpResponse::setContentType('application/json; charset=utf-8');
-        \HttpResponse::setData($responseData);
+        \HttpResponse::setData($responseJSON);
 
         return \HttpResponse::send();
     }
