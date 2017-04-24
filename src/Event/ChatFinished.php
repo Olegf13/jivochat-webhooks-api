00564bce35a31533aea49fba852f0d2dcd18c66d
Oleg Fedorov <olegf39@gmail.com>
2017-04-24 14:49:00 +0300

    Implement `ChatFinished` event class.

---
+++ src/Event/ChatFinished.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Jivochat\Webhooks\Event;
+
+use Jivochat\Webhooks\Request\Agent;
+use Jivochat\Webhooks\Request\Chat;
+
+/**
+ * Class ChatFinished
+ * @package Jivochat\Webhooks\Event
+ */
+class ChatFinished extends Event
+{
+    /** @var int Chat id (e.g. 7180). */
+    protected $chat_id;
+    /** @var Chat Data on completed chatting. See {@link Chat} for details. */
+    protected $chat;
+    /** @var Agent[] Agents list. See {@link Agent} for details. */
+    protected $agents;
+
+    /**
+     * Setter for {@link agents} property.
+     *
+     * @param array $agents
+     * @throws \InvalidArgumentException
+     */
+    public function setAgents(array $agents)
+    {
+        /** @var Agent $agent */
+        foreach ($agents as $data) {
+            if (is_array($data)) {
+                $agent = new Agent();
+                $agent->populate($data);
+                $this->agents[] = $agent;
+                continue;
+            }
+
+            if ($data instanceof Agent) {
+                $this->agents[] = $data;
+                continue;
+            }
+
+            throw new \InvalidArgumentException('Invalid data given.');
+        }
+    }
+
+    /**
+     * Setter for {@link chat} property.
+     *
+     * @param Agent|array $data
+     * @throws \InvalidArgumentException
+     */
+    public function setChat($data)
+    {
+        if (is_array($data)) {
+            $chat = new Chat();
+            $chat->populate($data);
+            $this->chat = $chat;
+            return;
+        }
+
+        if ($data instanceof Chat) {
+            $this->chat = $data;
+            return;
+        }
+
+        throw new \InvalidArgumentException('Invalid data given.');
+    }
+}
